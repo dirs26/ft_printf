b427a8db8b8408c85e3312ebaa198ed086513a3f
DRS <dirs26@gmail.com>
2024-11-01 14:52:24 +0100

    Makefile, puthex, putptr are done

--- Functions/ft_printf_puthex.c
+++ Functions/ft_printf_puthex.c
@@ -1,26 +1,30 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libftprintf.h                                      :+:      :+:    :+:   */
+/*   ft_printf_puthex.c                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: diegrod2 <diegrod2@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/10/31 13:41:06 by diegrod2          #+#    #+#             */
-/*   Updated: 2024/11/01 14:36:00 by diegrod2         ###   ########.fr       */
+/*   Created: 2024/11/01 13:16:10 by diegrod2          #+#    #+#             */
+/*   Updated: 2024/11/01 13:31:49 by diegrod2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifdef libftprintf_h
-# define libftprintf_h
+#include "libftprintf.h"
 
-# include <stdarg.h>
-# include <stdlib.h>
-# include <unistd.h>
 
-int	ft_putbnr(int n);
-int	ft_putunbr(unsigned int n);
-int	ft_putchar(char c);
-int	ft_puthex(unsigned long int n, char format);
-int	ft_putptr(void *ptr);
+int	ft_puthex(unsigned long int n, char format)
+{
+	int		len = 0;
+	char	*b;
 
-#endif
+	if (format == 'x')
+		b = "0123456789abcdef";
+	else
+		b = "0123456789ABCDEF";
+
+	if (n >= 16)
+		len += ft_puthex(n / 16, format);
+	len += ft_putchar(b[n % 16]);
+	return (len);
+}
--- Functions/ft_printf_putptr.c
+++ Functions/ft_printf_putptr.c
@@ -1,26 +1,25 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libftprintf.h                                      :+:      :+:    :+:   */
+/*   ft_printf_putptr.c                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: diegrod2 <diegrod2@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/10/31 13:41:06 by diegrod2          #+#    #+#             */
-/*   Updated: 2024/11/01 14:36:00 by diegrod2         ###   ########.fr       */
+/*   Created: 2024/11/01 14:14:03 by diegrod2          #+#    #+#             */
+/*   Updated: 2024/11/01 14:35:28 by diegrod2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifdef libftprintf_h
-# define libftprintf_h
+#include "libftprintf.h"
 
-# include <stdarg.h>
-# include <stdlib.h>
-# include <unistd.h>
+int	ft_putptr(void *ptr)
+{
+	int	len;
 
-int	ft_putbnr(int n);
-int	ft_putunbr(unsigned int n);
-int	ft_putchar(char c);
-int	ft_puthex(unsigned long int n, char format);
-int	ft_putptr(void *ptr);
-
-#endif
+	len = 0;
+	if(!ptr)
+		return (ft_putstr("(nein)"));
+	len += ft_putstr("0x");
+	len += ft_putstr((unsigned long)ptr, 'x');
+	return (len);
+}
