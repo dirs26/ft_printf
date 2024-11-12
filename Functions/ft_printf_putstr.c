9803d42c666ea31aea748ef0eb3f98701193d206
Rodrigues <dirs26@gmail.com>
2024-11-12 16:42:40 +0100

    aa

--- Functions/ft_printf_putstr.c
+++ Functions/ft_printf_putstr.c
@@ -1,25 +1,28 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_printf_putptr.c                                 :+:      :+:    :+:   */
+/*   ft_printf_putstr.c                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: diegrod2 <diegrod2@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/11/01 14:14:03 by diegrod2          #+#    #+#             */
-/*   Updated: 2024/11/01 14:35:28 by diegrod2         ###   ########.fr       */
+/*   Created: 2024/11/12 16:07:37 by diegrod2          #+#    #+#             */
+/*   Updated: 2024/11/12 16:07:37 by diegrod2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int	ft_printf_putptr(void *ptr)
+int	ft_printf_putstr(char *str)
 {
 	int	len;
 
 	len = 0;
-	if(!ptr)
-		return (ft_putstr("(NIL)"));
-	len += ft_putstr("0x");
-	len += ft_putstr((unsigned long)ptr, 'x');
+	if (!str)
+		str = "(null)";
+	while (str[len])
+	{
+		write(1, &str[len], 1);
+		len++;
+	}
 	return (len);
 }
