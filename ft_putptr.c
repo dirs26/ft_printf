02fe9ecc9da8d0513f08aa350bb4fd0a7efd4816
Rodrigues <dirs26@gmail.com>
2024-11-18 12:21:26 +0100

    sa

--- ft_putptr.c
+++ ft_putptr.c
@@ -20,7 +20,7 @@ int	ft_putptr(void *ptr)
 	if (!ptr)
 		return (ft_putstr("(nil)"));
 	len += ft_putstr("0x");
-	len += ft_puthex((unsigned long long)ptr, 'x');
+	len += ft_puthex((unsigned long)ptr, 'x');
 	return (len);
 }
 
